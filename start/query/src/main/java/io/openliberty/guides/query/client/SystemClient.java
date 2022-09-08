d62449f84ae5fd759ad283b6e7c54cd6efd2180c
Gilbert Kwan <gkwan@ca.ibm.com>
2022-09-08 12:33:09 -0400

    first commit

---
+++ start/query/src/main/java/io/openliberty/guides/query/client/SystemClient.java
@@ -0,0 +1,31 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.query.client;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public interface SystemClient extends AutoCloseable {
+
+    @GET
+    @Path("/property/{property}")
+    @Produces(MediaType.TEXT_PLAIN)
+    String getProperty(@HeaderParam("Authorization") String authHeader,
+                       @PathParam("property") String property);
+
+}
