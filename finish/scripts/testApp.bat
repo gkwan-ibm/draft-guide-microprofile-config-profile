9f4bf203e936a4ceddeab32494825e4e7d5af5b9
Gilbert Kwan <gkwan@ca.ibm.com>
2022-11-11 17:19:09 -0500

    Dev content review

--- finish/scripts/testApp.bat
+++ finish/scripts/testApp.bat
@@ -2,6 +2,7 @@ mvn -pl system clean package liberty:create liberty:install-feature liberty:depl
 mvn -pl query clean package liberty:create liberty:install-feature liberty:deploy
 mvn -pl system liberty:start
 mvn -pl query liberty:start
+mvn -pl system failsafe:integration-test
 mvn -pl query failsafe:integration-test
 mvn -pl query liberty:stop
 mvn -pl system liberty:stop
