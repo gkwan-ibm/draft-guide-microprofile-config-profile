43164a7d284e93bbb17638357397ebc0e234d002
Gilbert Kwan <gkwan@ca.ibm.com>
2022-11-14 17:12:26 -0500

    Dev content review - separating 2 guides

--- finish/scripts/testApp.sh
+++ finish/scripts/testApp.sh
@@ -1,8 +1,16 @@
+# tag::build[]
 mvn -pl system clean package liberty:create liberty:install-feature liberty:deploy
 mvn -pl query clean package liberty:create liberty:install-feature liberty:deploy
+# end::build[]
+# tag::start[]
 mvn -pl system liberty:start
 mvn -pl query liberty:start
+# end::start[]
+# tag::test[]
 mvn -pl system failsafe:integration-test
 mvn -pl query failsafe:integration-test
+# end::test[]
+# tag::stop[]
 mvn -pl query liberty:stop
 mvn -pl system liberty:stop
+# end::stop[]
